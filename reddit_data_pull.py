12802deac1f881bc0e90cef46328fc2b15178d3b
artemt42 <a.tishakov1@gmail.com>
2022-03-26 17:01:59 +0100

    updated reddit scripts

--- reddit_data_pull.py
+++ reddit_data_pull.py
@@ -1,29 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 26 15:59:03 2022
+Created on Sat Mar 26 16:43:03 2022
 
 @author: atishakov
 """
 
+import reddit_api_key_request as new_key
+import time
+import json
 
-import requests
-import os
+def api_key_check():
+    print(new_key.request())
+    with open('api_keys/reddit_api.txt',"r") as f:
+        api_reddit = f.read()
+    api_reddit = json.loads(api_reddit)
+    api_key = api_reddit["access_token"]
+    return api_key
 
-def api_request():
-    app_id = os.getenv('reddit_app_id')
-    secret = os.getenv('reddit_app_secret')
-    auth = requests.auth.HTTPBasicAuth(app_id, secret)
-    reddit_username = os.getenv('reddit_username')
-    reddit_password = os.getenv('reddit_password')
-    data = {
-            'grant_type': 'password',
-            'username': reddit_username,
-            'password': reddit_password
-            }
-    headers = {'User-Agent': 'NFT_Thesis/0.0.1'}
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-    auth=auth, data=data, headers=headers)
-    return res
-
-api = api_request()
-api.text
+api_key_check()
