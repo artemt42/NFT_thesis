2c49534d53cdecd3c80f2d00f1543d4c5658d213
artemt42 <a.tishakov1@gmail.com>
2022-03-26 17:29:18 +0100

    update

--- reddit_data_pull.py
+++ reddit_data_pull.py
@@ -8,13 +8,54 @@ Created on Sat Mar 26 16:43:03 2022
 import reddit_api_key_request as new_key
 import time
 import json
+import os
+import requests
 
-def api_key_check():
-    print(new_key.request())
+app_id = os.getenv('reddit_app_id')
+secret = os.getenv('reddit_app_secret')
+reddit_username = os.getenv('reddit_username')
+reddit_password = os.getenv('reddit_password')
+auth = requests.auth.HTTPBasicAuth(app_id, secret)
+headers = {'User-Agent': 'NFT_Thesis/0.0.1'}
+data = {
+        'grant_type': 'password',
+        'username': reddit_username,
+        'password': reddit_password
+        }
+
+def js(d):
+    return json.loads(d)
+
+def api_file():
     with open('api_keys/reddit_api.txt',"r") as f:
         api_reddit = f.read()
     api_reddit = json.loads(api_reddit)
-    api_key = api_reddit["access_token"]
-    return api_key
+    return api_reddit
 
-api_key_check()
+def api_request():
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+    auth=auth, data=data, headers=headers)
+    api = json.loads(res.text)
+    api['time_updated'] = int(round(time.time(),0))
+    api = json.dumps(api)
+    with open('api_keys/reddit_api.txt',"w") as f:
+        f.write(api)
+
+def api_key_check(force = False):
+    api_reddit = api_file()
+    curr_time = int(round(time.time(),0))
+    api_req_time = api_reddit['time_updated']
+    expires_in = api_reddit['expires_in']
+    to_request = (curr_time - api_req_time) > expires_in
+    if to_request and force:
+        new_key.api_request()
+        print("New key retrieval successful!")
+        api_reddit = api_file()
+    else:
+        print("Using the same key...")
+    return api_reddit['access_token']
+
+
+token = api_key_check()
+headers['Authorization'] = 'bearer {}'.format(token)
+t = requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
